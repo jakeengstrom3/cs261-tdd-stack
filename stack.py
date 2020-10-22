b53fa203a666ac6c95ce2f98d5a656127616fa29
Jacob Engstrom <jakeengstrom3@gmail.com>
2020-10-21 21:23:45 -0700

    First commit, all 11 Tests OK

--- stack.py
+++ stack.py
@@ -4,4 +4,25 @@
 
 class Stack:
 
-    pass
+    def __init__(self):
+        self.data = []
+
+    def is_empty(self):
+        return len(self.data) == 0
+
+    def pop(self):
+        if self.is_empty():
+            raise IndexError("Cannon Pop from empty Stack")
+        val = self.data[-1]
+        del self.data[-1]
+        return val
+        
+
+    def peek(self):
+        if self.is_empty():
+            raise IndexError("Cannon Peek an empty Stack")
+        return self.data[-1]
+
+    def push(self, val):
+        self.data.append(val)
+            
